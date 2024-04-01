b10a2d3f970a365ed304bd65f700c653db8d5fb8
Victor Whyte <victordavidwhyte@gmail.com>
2024-04-01 03:45:06 +0100

    chore: download canvas

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,7 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useSnapshot } from "valtio";
 
-import config from "../config/config";
 import state from "../store";
 import { download } from "../assets";
 import { downloadCanvasToImage, reader } from "../config/helpers";
@@ -170,6 +169,13 @@ const Customizer = () => {
                 isActiveTab={activeFilterTab[tab.name]}
               />
             ))}
+            <button className="download-btn" onClick={downloadCanvasToImage}>
+              <img
+                src={download}
+                alt="download_image"
+                className="w-3/5 h-3/5 object-contain"
+              />
+            </button>
           </motion.div>
         </>
       )}
