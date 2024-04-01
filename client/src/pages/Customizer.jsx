bb7d609f139c2bb428a023d1b5166cbcad19fa5b
Victor Whyte <victordavidwhyte@gmail.com>
2024-04-01 07:52:29 +0100

    chore: switch api

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -56,15 +56,18 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        "https://threejs-ai-merch.onrender.com/api/v1/dalle",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
 
       console.log({ response });
 
